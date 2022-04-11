1fc1b89009377b234c5dc1828fd9dbce3b8615fc
Yago Araújo <95325543+yagoww@users.noreply.github.com>
2022-04-11 16:00:27 -0300

    Finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
 const ul = document.querySelector('.containerListaProdutos ul');
-
+const listaCarrinho = document.querySelector('#listaCarrinho')
 
 function montarListaProdutos(listaProdutos) {
+    const precoTotal = document.getElementById("precoTotal");
+    let precoItens = 0;
 
     ul.innerHTML = '';
-    
+
     listaProdutos.forEach((produto) => {
         const li = document.createElement('li');
         const img = document.createElement('img');
@@ -20,20 +22,42 @@ function montarListaProdutos(listaProdutos) {
             ol.appendChild(itemComponentes);
         }
 
-        const btnCarrinho = document.createElement('button');
-        btnCarrinho.classList.add = 'btnCarrinho';
-        btnCarrinho.innerText = 'Adicionar ao Carrinho';
-        btnCarrinho.id = produto.id;
-
         img.src = produto.img;
         img.alt = produto.nome;
         h3.innerText = produto.nome;
         p.innerText = produto.preco;
         span.innerText = produto.secao;
 
+        const btnCarrinho = document.createElement('button');
+        btnCarrinho.classList.add = 'btnCarrinho';
+        btnCarrinho.innerText = 'Adicionar ao Carrinho';
+        btnCarrinho.id = produto.id;
+        
+        btnCarrinho.addEventListener("click", () => {
+            const li = document.createElement("li");
+            const img = document.createElement('img');
+            const nome = document.createElement("span");
+            const preco = document.createElement("span");
+            const secao = document.createElement("span");
+            
+            img.src = produto.img;
+            img.alt = produto.nome;
+            nome.innerText = produto.nome;
+            preco.innerText = produto.preco;
+            secao.innerText = produto.secao;
+
+            li.appendChild(img)
+            li.appendChild(nome)
+            li.appendChild(preco)
+            li.appendChild(secao)
+            listaCarrinho.appendChild(li);
+
+            precoItens += Number.parseFloat(produto.preco);
+            precoTotal.innerHTML = precoItens.toFixed(2).replace(".", ",");
+        })
+
         li.append(img, h3, p, span, ol, btnCarrinho);
         ul.appendChild(li);
-
     });
 
 }
